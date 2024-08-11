9f21a6f15574622db5648ec86ad1fcd1b3866c90
thalyshenrique7 <thalys_henrique@outlook.com>
2024-08-11 14:56:21 -0300

    structuring microservices dto classes

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/OrderProducts.java
@@ -0,0 +1,15 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderProducts {
+
+    private Product product;
+    private int quantity;
+}
