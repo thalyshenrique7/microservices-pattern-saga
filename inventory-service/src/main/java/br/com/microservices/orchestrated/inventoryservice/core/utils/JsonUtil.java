247fa7157c710b7fd1c61f51cb6865345b948a8a
thalyshenrique7 <thalys_henrique@outlook.com>
2024-08-11 15:11:04 -0300

    structuring json utility and exception configuration

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/utils/JsonUtil.java
@@ -0,0 +1,34 @@
+package br.com.microservices.orchestrated.inventoryservice.core.utils;
+
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object obj) {
+
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
